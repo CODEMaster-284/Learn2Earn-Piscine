222a771003d863da5dffa2164d413bf9a2ecbc37
pawofeso <awofesobipeace@gmail.com>
2026-04-19 12:56:45 +0100

    updated the syntax

--- myfamily.sh
+++ myfamily.sh
@@ -1,1 +1,1 @@
-curl -s https://acad.learn2earn.ng/assets/superhero/all.json |jq -r --arg id "$HERO_ID" '.[] |select((.id|tostring) ==$id) | .connections.relatives '
+curl -s https://acad.learn2earn.ng/assets/superhero/all.json |jq -r --arg id "$HERO_ID" '.[] |select((.id|tostring) ==$id) | .connections.relatives | gsub("\\n"; "\n")'
