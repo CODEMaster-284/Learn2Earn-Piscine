0f1ed4f917644b4ce0b97ce79f3d80f378dfe783
pawofeso <awofesobipeace@gmail.com>
2026-04-20 23:41:26 +0100

    reformated the file

--- printcomb2.go
+++ printcomb2.go
@@ -1,8 +1,8 @@
-package main
+package piscine
 
 import "github.com/01-edu/z01"
 
-func main() {
+func PrintComb2() {
 	for first := 10; first <= 98; first++ {
 		for second := first + 1; second <= 99; second++ {
 			z01.PrintRune(rune('0' + first/10))
