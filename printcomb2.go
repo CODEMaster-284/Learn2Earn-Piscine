78521bc7b944369aceab9efee6a4482702b5d178
pawofeso <awofesobipeace@gmail.com>
2026-04-20 23:35:45 +0100

    added the print comb2 file

---
+++ printcomb2.go
@@ -0,0 +1,21 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func main(){
+	for first:=10; first<=98; first++{
+		for second:=first+1; second<=99; second++ {
+			z01.PrintRune(rune('0' + first/10))
+			z01.PrintRune(rune('0' + first%10))
+			z01.PrintRune(' ')
+			z01.PrintRune(rune('0' + second/10))
+			z01.PrintRune(rune('0' + second%10))
+
+			if first!=98 || second!=99 {
+				z01.PrintRune(',')
+				z01.PrintRune(' ')
+			}
+		}
+	}
+	z01.PrintRune('\n')
+}
