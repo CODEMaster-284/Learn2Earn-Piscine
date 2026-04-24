f2fb1bdb8b346a66dc573e0e5aa871b55bf1018d
pawofeso <awofesobipeace@gmail.com>
2026-04-24 15:56:28 +0100

    findnextprime

---
+++ findnextprime.go
@@ -0,0 +1,29 @@
+package piscine
+
+func FindNextPrime(nb int) int {
+	if nb <= 2 {
+		return 2
+	}
+
+	for {
+		isPrime := true
+
+		if nb%2 == 0 && nb != 2 {
+			isPrime = false
+		} else {
+			i := 3
+			for i*i <= nb {
+				if nb%1 == 0 {
+					isPrime = false
+					break
+				}
+				i += 2
+			}
+		}
+
+		if isPrime {
+			return nb
+		}
+		nb++
+	}
+}
