de814bbb0bf5591156213096f2f8a769ac69894c
pawofeso <awofesobipeace@gmail.com>
2026-04-26 19:45:04 +0100

    nbr

---
+++ nbrconvertalpha/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	args := os.Args[1:]
+	upper := false
+	start := 0
+
+	// Check for --upper flag
+	if len(args) > 0 && args[0] == "--upper" {
+		upper = true
+		start = 1
+	}
+
+	for i := start; i < len(args); i++ {
+		n := atoi(args[i])
+
+		if n >= 1 && n <= 26 {
+			var ch rune
+			if upper {
+				ch = rune('A' + n - 1)
+			} else {
+				ch = rune('a' + n - 1)
+			}
+			z01.PrintRune(ch)
+		} else {
+			z01.PrintRune(' ')
+		}
+	}
+
+	z01.PrintRune('\n')
+}
+
+// Convert string to int (returns 0 if invalid)
+func atoi(s string) int {
+	n := 0
+
+	for _, ch := range s {
+		if ch < '0' || ch > '9' {
+			return 0
+		}
+		n = n*10 + int(ch-'0')
+	}
+
+	return n
+}
