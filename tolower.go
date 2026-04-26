83094c87ef80508a7c5fdd9ac7f11c5e33695dcd
pawofeso <awofesobipeace@gmail.com>
2026-04-26 10:29:47 +0100

    tolower

--- tolower.go
+++ tolower.go
@@ -1,1 +1,17 @@
 package piscine
+
+func ToLower(s string) string {
+	result := ""
+
+	for i := 0; i < len(s); i++ {
+		c := s[i]
+
+		if c >= 'A' && c <= 'Z' {
+			c = c + 32
+		}
+
+		result += string(c)
+	}
+
+	return result
+}
