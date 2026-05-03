8d6633e95e3ebc50aa17738bdcb119eefa80bde8
pawofeso <awofesobipeace@gmail.com>
2026-05-03 15:27:45 +0100

    sortwordarr

--- sortwordarr.go
+++ sortwordarr.go
@@ -1,26 +1,27 @@
 package piscine
 
 func SortWordArr(a []string) {
-	n := len(a)
-
-	for i := 0; i < n-1; i++ {
-		for j := 0; j < n-i-1; j++ {
-			if compare(a[j], a[j+1]) > 0 {
+	for i := 0; i < len(a)-1; i++ {
+		for j := 0; j < len(a)-1-i; j++ {
+			if greater(a[j], a[j+1]) {
 				a[j], a[j+1] = a[j+1], a[j]
 			}
 		}
 	}
 }
 
-func compare(s1, s2 string) int {
+func greater(s1, s2 string) bool {
 	i := 0
 
 	for i < len(s1) && i < len(s2) {
-		if s1[i] != s2[i] {
-			return int(s1[i] - s2[i])
+		if s1[i] > s2[i] {
+			return true
+		}
+		if s1[i] < s2[i] {
+			return false
 		}
 		i++
 	}
 
-	return len(s1) - len(s2)
+	return len(s1) > len(s2)
 }
