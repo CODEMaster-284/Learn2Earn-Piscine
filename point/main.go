d929ce4afb325c656c58751506bd681e2079c393
pawofeso <awofesobipeace@gmail.com>
2026-05-03 12:57:50 +0100

    formatted the point file

--- point/main.go
+++ point/main.go
@@ -1,11 +1,17 @@
-package piscine
+package main
 
-type point struct {
-	x int
-	y int
-}
+import "github.com/01-edu/z01"
 
-func setPoint(ptr *point) {
-	ptr.x = 42
-	ptr.y = 21
+type point struct{ x, y int }
+
+func setPoint(ptr *point) { ptr.x, ptr.y = 42, 21 }
+
+func main() {
+	var p point
+	setPoint(&p)
+
+	s := []rune(120, 32, 61, 32, 52, 50, 44, 32, 121, 32, 61, 32, 50, 49, 10)
+	for i := 0; i < 15; i++ {
+		z01.PrintRune(s[i])
+	}
 }
