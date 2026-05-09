f72aecfa08297fe63de63dbf0581d7868bafae02
pawofeso <awofesobipeace@gmail.com>
2026-05-09 13:37:05 +0100

    listlast

---
+++ listlast.go
@@ -0,0 +1,18 @@
+package piscine
+
+func ListLast(l *List) interface{} {
+	if l == nil || l.Head == nil {
+		return nil
+	}
+
+	if l.Tail != nil {
+		return l.Tail.Data
+	}
+
+	node := l.Head
+	for node.Next != nil {
+		node = node.Next
+	}
+
+	return node.Data
+}
