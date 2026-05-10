3c9ba5feb532cda31886c6383d11ead7cb05118d
pawofeso <awofesobipeace@gmail.com>
2026-05-10 13:13:02 +0100

    listforeach

---
+++ listforeach.go
@@ -0,0 +1,29 @@
+package piscine
+
+func ListForEach(l *List, f func(*NodeL)) {
+	if l == nil || f == nil {
+		return
+	}
+
+	for node := l.Head; node != nil; node = node.Next {
+		f(node)
+	}
+}
+
+func Add2_node(node *NodeL) {
+	switch node.Data.(type) {
+	case int:
+		node.Data = node.Data.(int) + 2
+	case string:
+		node.Data = node.Data.(string) + "2"
+	}
+}
+
+func Subtract3_node(node *NodeL) {
+	switch node.Data.(type) {
+	case int:
+		node.Data = node.Data.(int) - 3
+	case string:
+		node.Data = node.Data.(string) + "-3"
+	}
+}
